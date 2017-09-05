ab19938e244a3f63e03db7daecebf2439c64d119
aaku14n <akarshgdps@gmail.com>
2017-09-05 15:02:33 +0530

    aaku branch

--- src/component/user.js
+++ src/component/user.js
@@ -1,25 +1,12 @@
 import React from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-<<<<<<< HEAD
-import { selectAction } from "../action/index";
-=======
-
->>>>>>> b46718e1803eb9ab0e5bb6ea867ff625bb4ad074
 class User extends React.Component {
   print(user) {
     return <h1 onClick={() => this.props.selectAction(user)}>{user.id}</h1>;
   }
   render() {
-<<<<<<< HEAD
-<<<<<<< HEAD
     return <div>{this.props.user.map(post => this.print(post))}</div>;
-=======
-    return <div>data</div>;
->>>>>>> current
-=======
-    return <div>data main</div>;
->>>>>>> 8e179fab57ce01b07bd8b5f13ddce62f834e5c7a
   }
 }
 function mapStateToProps(state) {
